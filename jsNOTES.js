c41d313511b4d98a16cfd96d4e30d3bd33a4c7ad
C Lewis <casserole.lewis@gmail.com>
2023-03-03 15:01:05 -0500

    JavaScript refactor

--- jsNOTES.js
+++ jsNOTES.js
@@ -1,5 +1,3 @@
-/****** VARIABLES ******/
-
 //CHARACTER ARRAYS
 const charGroup = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z",
 "a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
@@ -37,20 +35,31 @@ function randomPassword () {
 
   let pw = charGroup;
 
+  //??? .concat(): https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/concat
+  //??? Is this the best way?
+
   if (inclNumbers.checked) {
+    //! if numbers checkbox is checked, .concat() combines pw/char array with number checkbox
     pw = pw.concat(numGroup); 
+    // pw += numGroup;
   };
 
   if (inclSymbols.checked) {
+    //! if symbols checkbox is checked, .concat() combines pw/char array with symbol checkbox
     pw = pw.concat(symGroup);
+    // pw += symGroup;
   };
 
+  //!iterate based on length of input range selection
   for (let i = 0; i < inputRange.value; i++) {
 
     let randomCharOne = Math.floor(Math.random() * pw.length);
     let randomCharTwo = Math.floor(Math.random() * pw.length);
-
     resultOne.value += pw[randomCharOne];
+
+    //!assigns value to text input field
+    //! += adds a random value based on input selections every iteration / length of range
+    
     resultTwo.value += pw[randomCharTwo];
 
   };
@@ -58,15 +67,22 @@ function randomPassword () {
 
 function copyPasswordOne () {
   let copyText = resultOne;
+  //! .select() method selects text from an input field
   copyText.select();
+  //! writes to system clipboard
+  //??? site permissions to use clipboard? Is this secure? https://developer.mozilla.org/en-US/docs/Web/API/Navigator/clipboard
   navigator.clipboard.writeText(copyText.value);
 
+  //! create a div and add it to the document
   const alert = document.createElement("div");
+  //! add the alert class and text
   alert.classList.add("alert");
   alert.textContent = "Copied to clipboard";
+   //! add alert element to the body as child node 
   document.body.appendChild(alert);
   
   setTimeout(function() {
+    //! remove the alert from the document after 1000ms
       document.body.removeChild(alert);
   }, 1000);
   return resultOne;
@@ -97,4 +113,6 @@ generateBtn.addEventListener("click", randomPassword)
 copyPwOne.addEventListener("click", copyPasswordOne);
 copyPwTwo.addEventListener("click", copyPasswordTwo)
 
+//??? I got an accessibility error check that not all of my inputs are able to be activated by a keyboard (specifically the range)
+//??? What do you do besides "click" on an event listener to make sure the keyboard can be used as well?
 
